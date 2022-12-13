3fd9438ff2433d47bd3ae0743b883fb3e160a275
Shola Jegede <83558130+sholajegede@users.noreply.github.com>
2022-12-13 15:36:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -9,9 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Set static folder
-app.use(express.static(path.join(__dirname, "public")));
-
 app.use("/openai", require("./routes/openaiRoutes"));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
